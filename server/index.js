1aab8c5daf50e90bdaa249cce5aff69e62513bf6
Tushar Suryawanshi <tushar.823737@gmail.com>
2024-01-30 18:43:26 +0530

    connect to DB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error(err);
+});
 
 const app = express()
 
 app.listen(3000, () => {
     console.log('Server listing on port 3000')
 })
+
