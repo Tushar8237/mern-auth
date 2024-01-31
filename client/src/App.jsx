8edf9edce86352665d6c0756c892cde2dbd2fd61
Tushar Suryawanshi <tushar.823737@gmail.com>
2024-01-31 14:09:15 +0530

    add redux toolkit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
-import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import SignIn from "./pages/SignIn";
 
 export default function App() {
   return (
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,43 +1,44 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
-export default function SignIp() {
-
-  const [formData, setFormData]=useState({})
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
-  
+  const dispatch = useDispatch();
+
   const HandleChange = (e) => {
-    setFormData({...formData, [e.target.id] : e.target.value})
-  }
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-
+    e.preventDefault();
     try {
-      setLoading(true)
-      setError(false)
-
-      const res = await fetch('/api/auth/signin',{
-        method : 'POST',
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
         headers: {
-          'Content-type': 'application/json',
+          "Content-type": "application/json",
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
-      const data = await res.json()
-      setLoading(false)
-      if(data.success === false){
-        setError(true)
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data));
         return;
       }
-      navigate('/');
+      dispatch(signInSuccess(data));
+      navigate("/");
     } catch (error) {
-      setLoading(false)
-      setError(true)
+      dispatch(signInFailure(error));
     }
-  }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -62,7 +63,7 @@ export default function SignIp() {
           className="bg-slate-700 text-white p-3 
           rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
-          {loading ? 'Loading...' : 'Sign In'}
+          {loading ? "Loading..." : "Sign In"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -71,7 +72,9 @@ export default function SignIp() {
           <span className="text-blue-500">Sign up</span>
         </Link>
       </div>
-      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
+      <p className="text-red-700 mt-5">
+        {error ? error.message || "Something went wrong!" : ""}
+      </p>
     </div>
   );
 }
